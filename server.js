3c10ddf8f81ebf89ba1d780497499b35407fa1a1
iAtif <atifnadeem630@gmail.com>
2024-05-27 18:00:18 +0500

    Cors for Vercel

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ db.connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://socialmediatask.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.static("public/images"));
 
 // Define the root route
